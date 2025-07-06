32d6185dbb34c8a1077f14592d3807d171d1e0a2
Indira Makaraju <IndiraM@ssnc-corp.global>
2025-07-06 22:09:25 +1000

    Catalog microservice setup with Mapster, Carter and MediatR. Create POST API of Create product

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,19 @@
+using Catalog.API.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
